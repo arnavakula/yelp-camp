a50380d81ca2adcce23d9c0c5e533ec7150db9c6
Arnav Akula <34948408+arnavakula@users.noreply.github.com>
2024-07-11 13:25:09 -0500

    add redirect for user after logging in

--- middleware.js
+++ middleware.js
@@ -1,10 +1,18 @@
 const isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()){
+        req.session.returnTo = req.originalUrl;
         req.flash('error', 'Error: you must be signed in first!');
         return res.redirect('/login');
     }
 
     next();
 }
+const storeReturnTo = (req, res, next) => {
+    if (req.session.returnTo) {
+        res.locals.returnTo = req.session.returnTo;
+    }
+    next();
+}
 
 module.exports.isLoggedIn = isLoggedIn;
+module.exports.storeReturnTo = storeReturnTo;
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
 const passport = require('passport')
+const { storeReturnTo } = require('../middleware');
 
 const router = express.Router();
 
@@ -11,13 +12,20 @@ router.get('/register', (req, res) => {
 })
 
 //create new user
-router.post('/register', catchAsync(async (req, res) => {
+router.post('/register', catchAsync(async (req, res, next) => {
     try{
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
-        req.flash('success', 'Welcome to Camp Sage!');
-        res.redirect('/campgrounds');
+        req.login(registeredUser, err => {
+            if(err){
+                return next(err);
+            }
+            req.flash('success', `Welcome to CampSage ${username}`);
+            const redirectUrl = res.locals.returnTo || '/campgrounds';
+            res.redirect(redirectUrl);
+        })
+        
     } catch(err){
         req.flash('error', err.message);
         res.redirect('/register');
@@ -30,10 +38,12 @@ router.get('/login', (req, res) => {
 })
 
 //authenticate user
-router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), catchAsync(async (req, res) => {
+router.post('/login', storeReturnTo, passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}),(req, res) => {
     req.flash('success', 'Succesfully logged in, welcome!');
-    res.redirect('/campgrounds');
-}))
+    const redirectUrl = res.locals.returnTo || 'campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
+})
 
 //logout
 router.get('/logout', (req, res, next) => {
